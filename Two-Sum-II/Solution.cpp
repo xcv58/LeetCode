e9378d2a96539342bced41f5b7e217af27bf5411
xcv58 <chenyihonglove@gmail.com>
2014-12-19 01:30:07 -0500

    update c++ solution for Two-Sum-II

--- Two-Sum-II/Solution.cpp
+++ Two-Sum-II/Solution.cpp
@@ -5,11 +5,16 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
+        vector<int> result (2, -1);
         for (int i = 0, j = numbers.size() - 1, gap; i < j; j = gap > 0 ? j - 1 : j, i = gap > 0 ? i : i + 1) {
             gap = numbers[i] + numbers[j] - target;
-            if (gap == 0) { return {i + 1, j + 1}; }
+            if (gap == 0) {
+                result[0] = i + 1;
+                result[1] = j + 1;
+                break;
+            }
         }
-        return {-1, -1};
+        return result;
     }
 };
 
