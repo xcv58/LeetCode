1a4a50ad36167aeb0bb660a4a368ef79419340c5
xcv58 <chenyihonglove@gmail.com>
2014-12-21 13:27:08 -0500

    update java solution for Excel-Sheet-Column-Title

--- Excel-Sheet-Column-Title/Solution.java
+++ Excel-Sheet-Column-Title/Solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public String convertToTitle(int n) {
         if (n == 0) { return ""; }
-        return convertToTitle((n - 1) / 26) + (char)('A' + (--n % 26));
+        return convertToTitle(--n / 26) + (char)('A' + (n % 26));
     }
 }
