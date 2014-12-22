f6abfc9b323fa58c5aa33dbbae275a3d1315487b
xcv58 <chenyihonglove@gmail.com>
2014-12-22 12:15:38 -0500

    update python solution for Majority-Element

--- Majority-Element/Solution.py
+++ Majority-Element/Solution.py
@@ -2,11 +2,4 @@ class Solution:
     # @param num, a list of integers
     # @return an integer
     def majorityElement(self, num):
-        index = 0
-        count = 1
-        for i in range(len(num)):
-            count += 1 if num[index] == num[i] else -1
-            if count == 0:
-                index = i
-                count = 1
-        return num[index]
+        return sorted(num)[len(num) / 2]
