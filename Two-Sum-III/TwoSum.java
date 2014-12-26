4fabd459fde6b722cf232ac2a91a030869ed86b2
xcv58 <chenyihonglove@gmail.com>
2014-12-26 00:11:23 -0500

    add solutions for Two-Sum-III

---
+++ Two-Sum-III/TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class TwoSum {
+    private HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+    public void add(int number) {
+        map.put(number, map.containsKey(number) ? map.get(number) + 1 : 1);
+    }
+
+    public boolean find(int value) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int i = entry.getKey();
+            int j = value - i;
+            if ((i == j && entry.getValue() > 1) || (i != j && map.containsKey(j))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
