9f337c81eb1c675210ac23a27034460216316f1f
xcv58 <chenyihonglove@gmail.com>
2014-12-29 01:47:38 -0500

    update C++ solutions for Two-Sum-III

--- Two-Sum-III/TwoSum.cpp
+++ Two-Sum-III/TwoSum.cpp
@@ -5,20 +5,20 @@ using namespace std;
 class TwoSum {
     unordered_map<int,int> map;
 public:
-        void add(int number) {
-            map[number] = map.find(number) == map.end() ? 1 : map[number] + 1;
-        }
+    void add(int number) {
+        map[number]++;
+    }
 
-        bool find(int value) {
+    bool find(int value) {
         for (unordered_map<int,int>::iterator it = map.begin(); it != map.end(); it++) {
-                int i = it->first;
-                int j = value - i;
-                if ((i == j && it->second > 1) || (i != j && map.find(j) != map.end())) {
-                    return true;
-                }
+            int i = it->first;
+            int j = value - i;
+            if ((i == j && it->second > 1) || (i != j && map.find(j) != map.end())) {
+                return true;
             }
-            return false;
         }
+        return false;
+    }
 };
 
 int main(int argc, char *argv[]) {
