5382b6230b2a97a54ed893b77344bee07559825b
xcv58 <i@xcv58.com>
2017-12-30 18:45:28 -0500

    add solution for Second-Minimum-Node-In-a-Binary-Tree

---
+++ Second-Minimum-Node-In-a-Binary-Tree/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findSecondMinimumValue(TreeNode root) {
+        return this.findLargeThan(root, root.val);
+    }
+
+    public int findLargeThan(TreeNode root, int target) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.val > target) {
+            return root.val;
+        }
+        int left = this.findLargeThan(root.left, target);
+        int right = this.findLargeThan(root.right, target);
+        if (left >= 0 && right >= 0) {
+            return Math.min(left, right);
+        }
+        return Math.max(left, right);
+    }
+}
