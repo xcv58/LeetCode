c872209c7241571c717f568e1067736d91a3e2e1
xcv58 <i@xcv58.com>
2017-12-31 17:35:56 -0500

    Add Java solution for Serialize-and-Deserialize-BST

---
+++ Serialize-and-Deserialize-BST/Solution.java
@@ -0,0 +1,46 @@
+public class Codec {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    private static final String DELIMITER = "-";
+    private static final String NULL = " ";
+    private int index = 0;
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return NULL;
+        }
+        return root.val + DELIMITER + this.serialize(root.left) + DELIMITER + this.serialize(root.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] nodes = data.split(this.DELIMITER);
+        this.index = 0;
+        return this.deserialize(nodes);
+    }
+    
+    private TreeNode deserialize(String[] nodes) {
+        if (index >= nodes.length) {
+            return null;
+        }
+        String node = nodes[this.index++];
+        if (node.equals(NULL)) {
+            return null;
+        }
+        int val = Integer.parseInt(node);
+        TreeNode root = new TreeNode(val);
+        root.left = this.deserialize(nodes);
+        root.right = this.deserialize(nodes);
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
